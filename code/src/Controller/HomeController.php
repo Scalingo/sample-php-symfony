321a2a698aa20047c015e3a31cb7571407d4cc85
Étienne Michon <etienne@scalingo.com>
2020-01-16 11:12:27 +0100

    Test Home controller

---
+++ code/src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+// namespace App\Controller;
+//
+// use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\Routing\Annotation\Route;
+//
+// class HomeController extends Controller
+// {
+//   /**
+//    * @Route("/lucky/number")
+//    */
+//   public function index()
+//   {
+//       return $this->render('index.html.twig');
+//   }
+// }
