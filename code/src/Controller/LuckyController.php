83aafa9cd9d4d0226d94c7a0f99b9412ffaaf039
Étienne Michon <etienne@scalingo.com>
2020-01-16 11:24:04 +0100

    Add the Lucky route from the tuto
    https://symfony.com/doc/current/page_creation.html\#creating-a-page-route-and-controller

---
+++ code/src/Controller/LuckyController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LuckyController extends AbstractController
+{
+     /**
+      * @Route("/lucky/number")
+      */
+    public function number()
+    {
+        $number = random_int(0, 100);
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
