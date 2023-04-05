6873c7489490c03f2ff239377e3b399e6da6a03d
aurelien-reeves-scalingo <aurelien.reeves@scalingo.com>
2023-04-05 09:37:53 +0200

    Add database stuff

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ProductController extends AbstractController
+{
+    // #[Route('/product', name: 'app_product')]
+    // public function index(): Response
+    // {
+    //     return $this->render('product/index.html.twig', [
+    //         'controller_name' => 'ProductController',
+    //     ]);
+    // }
+
+    #[Route('/product', name: 'create_product')]
+    public function createProduct(EntityManagerInterface $entityManager): Response
+    {
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(1999);
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+}
